59bc3dc93c9f6480b8f85d91674fa48ef64585ed
mhernandez0721 <mhernandez0721@fullsail.edu>
2014-05-15 17:59:21 -0400

    Update Hernandez_Michelle_Project2.js

--- sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/Hernandez_Michelle_Project2.js
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/Hernandez_Michelle_Project2.js
@@ -3,3 +3,27 @@
 //SD1405
 //Porject2
 
+<!DOCtype html>
+<html>
+<head>
+	<title> Project 2: Crocheting a beanie hat. </title>
+	<script type="text/javascript">
+	
+	
+var yarn(a) = "red";
+var yarn(b) = "blue";
+var hookSize = 5.0;
+var singleCrochet = true;
+
+	alert(yarn(a));
+		console.log("hookSize");
+		
+
+
+
+</html>
+</head>
+</script>
+
+
+
