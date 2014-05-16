72a48f0ffc4c40d0a9191f0bd63661a865c1d3ec
mhernandez0721 <mhernandez0721@fullsail.edu>
2014-05-15 22:15:05 -0400

    Update Hernandez_Michelle_Project2.js

--- Hernandez_Michelle_Project2.js
+++ Hernandez_Michelle_Project2.js
@@ -1,1 +1,50 @@
-alert("JavaScript works!");
+alert("My Crochet Beanie hat!");
+// Michelle Hernandez
+// SDI 1405
+// Project 2
+// My Crochet Beanie Hat
+
+//myVariables
+var myYarnColor = ("Red yarn");
+var myJHook = 5.0;
+var singleCrochet = true,
+
+myAction
+var otherStitch = ("HalfDoubleCrochet");
+var startCrochet = function(myAction) 
+
+myComparisons
+var hatSize = prompt("Enter Hat Size:", "");
+var largerThanLarge = parseInt(anySize) > anySize;
+var anySize = function(startCrochet)
+		console.log("startCrochet" + "largerThanLarge");
+		
+{
+	if (myAction === singleCrochet);
+		console.log("Grab" + " my J Hook " + ", my Yarn Color & chain 4 single crochet or");
+} 	
+		(myAction === otherStitch); {
+		console.log("Grab" + " my J Hook" + ",  my Yarn Color & chain 4 double crochet ");
+};
+
+myRound = function(stitch){
+var stitchTotal = 12;
+var stitch;
+	while (stitch <12){
+	stitch++;
+		if (stitchTotal > 0 && stitchTotal == 12)
+		{
+			console.log("stitchTotal ends round at 12");
+		}
+	}
+};
+
+//while-loop
+
+var totalStitch = 60
+while (totalStitch > 0) {
+		console.log(totalStitch + " single crochet or double crochet in each round.");
+		totalStitch--;
+};
+
+console.log("End of beanie.");
